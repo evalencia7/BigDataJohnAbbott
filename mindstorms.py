54f4740dff5095d77509286b76b2f166b1a2392a
evalencia7 <edison_valencia@yahoo.com>
2017-06-03 15:44:42 -0400

    Add files via upload

--- mindstorms.py
+++ mindstorms.py
@@ -7,6 +7,12 @@ def draw_square():
     brad = turtle.Turtle()
     brad.forward(100)
     brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
 
     window.exitonclick()
     
